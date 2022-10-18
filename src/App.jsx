117c4e4f250c5af9e514a0ed97ac1d3692c5f372
farhad-ali1501~ <farhadali15@yahoo.com>
2022-10-18 22:16:41 +0500

    added button handler

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,20 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 function App() {
+  let a = 90
+  let handleClick = () => {
+    console.log("hello");
+  }
   return (
     <>
-     <Header/>
-      <Main/>
-      <Footer/>
+
+
+      <div>
+        <button onClick={handleClick}>+</button>
+        <p>
+          {a * 2}
+        </p>
+      </div>
     </>
 
   );
