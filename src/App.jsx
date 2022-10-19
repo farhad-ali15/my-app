381fc6901d8725db2578c759c72696c7ac6bdafd
farhad-ali1501~ <farhadali15@yahoo.com>
2022-10-19 21:53:16 +0500

    use of ternary operator and useState()

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,35 @@ function App() {
   let [num, setNum] = useState(90)
 
 
-  function handleClick() {
-    setNum(num+1)
+  function handleInc() {
+    if (num>=100)  return;
+      setNum(num + 1)
   }
+  function handleDec() {
+    if (num <=80) return;
+    setNum(num - 1)
+  }
+
+  // if (num>=100){
+  //   console.log("greater than 100")
+  // }
+  // else{
+  //   console.log("less than 100")
+  // }
+  // num>=100 ? console.log("greater than 100") :console.log("less than 100")
+
+
   return (
     <>
 
 
       <div>
-        
+
         <p>
           {num}
         </p>
-        <button onClick={handleClick}>+</button>
+        <button disabled={num>=100 ? true:false} onClick={handleInc}>+</button>
+        <button disabled={num<=80 ? true:false} onClick={handleDec}>-</button>
       </div>
     </>
 
