fe2e9719d320f6506e6d8863e55e926f33be776b
farhad-ali1501~ <farhadali15@yahoo.com>
2022-10-19 21:07:18 +0500

    use state and virtual dom

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,26 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
+import { useState } from "react";
 
 function App() {
-  let a = 90
-  let handleClick = () => {
-    console.log("hello");
+  //let a = 90
+  let [num, setNum] = useState(90)
+
+
+  function handleClick() {
+    setNum(num+1)
   }
   return (
     <>
 
 
       <div>
-        <button onClick={handleClick}>+</button>
+        
         <p>
-          {a * 2}
+          {num}
         </p>
+        <button onClick={handleClick}>+</button>
       </div>
     </>
 
